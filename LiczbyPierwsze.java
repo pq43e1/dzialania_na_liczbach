070f5fe619edd9fdb828158bc554889ea1fee604
pq43e1 <76269570+pq43e1@users.noreply.github.com>
2021-10-11 16:51:11 +0200

    Create LiczbyPierwsze.java

---
+++ LiczbyPierwsze.java
@@ -0,0 +1,34 @@
+public class LiczbaPierwsza{
+
+     public static void main(String []args){
+         
+      int zakres = 6;
+      
+      for(int x = 1 ; x < zakres ; x++)
+      {
+        if(czyPierwsza(x)) System.out.print(x+", ");
+      }
+    }
+    
+    public static boolean czyPierwsza(int x)
+    {
+        int zakres;
+        if(x<5) zakres = x;
+        else zakres = x/2;
+        if(x<0) 
+        {
+            x = -x;
+        }
+        
+        for(int i = 2 ; i <zakres ; i++)
+        {
+            //System.out.println("Iteracja("+(i-1)+") "+x+"/"+i+"="+x/i);
+            if(x%i == 0)
+            {
+                i=x/2;
+                return false;
+            }
+        }
+        return true;
+    }
+}
